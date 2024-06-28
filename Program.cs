fefdfe6aee9e5164178fa6e9413eb97cedecfa04
Liam McCartney <liam.mccartney@gmail.com>
2024-06-28 09:58:51 -0400

    Hello World

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 400, "Hello World");
+
+while(!Raylib.WindowShouldClose())
+{
+  Raylib.BeginDrawing();
+  Raylib.ClearBackground(Color.White);
+
+  Raylib.DrawText("Hello, World!", 12, 12, 20, Color.Black);
+
+  Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
