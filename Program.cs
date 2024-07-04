63b1591b0e21f2474f2abecbb9990fa0ed097965
Liam McCartney <liam.mccartney@gmail.com>
2024-07-04 16:24:37 -0400

    Movement!!!

--- Program.cs
+++ Program.cs
@@ -2,64 +2,63 @@
 using Raylib_cs;
 using static Raylib_cs.Raylib;
 
-const int size = 32;
-const int screenWidth = 800;
-const int screenHeight = 450;
+const int size = 30;
 const float speed = size / 5;
 
-var square = (
-      position: new Vector2(screenWidth / 2, screenHeight / 2),
-      size: new Vector2(size, size),
-      speed: new Vector2(speed, 0),
-      color: Color.Black,
-      alive: true
-      );
+const int cellSize = 30;
+const int cellCount = 25;
+const int screenWidth = cellSize * cellCount;
+const int screenHeight = cellSize * cellCount;
+
+Vector2[] initPosition = { 
+  new Vector2((screenWidth / 2) - size - size - size, screenHeight / 2) ,
+  new Vector2((screenWidth / 2) - size - size, screenHeight / 2) ,
+  new Vector2((screenWidth / 2) - size, screenHeight / 2) ,
+  new Vector2(screenWidth / 2, screenHeight / 2), 
+};
+var snake = new List<Vector2>(initPosition);
+var right = new Vector2(size, 0);
+var left = new Vector2(-size, 0);
+var up = new Vector2(0, -size);
+var down = new Vector2(0, size);
+var direction = right;
+
 
 InitWindow(screenWidth, screenHeight, "Snake!");
 SetTargetFPS(60);
 
+var frame = 1;
 while (!WindowShouldClose())
 {
-  Update();
+  // -- Handle Input --
+  if (IsKeyDown(KeyboardKey.Right) && (direction == up || direction == down) ) direction = right;
+  if (IsKeyDown(KeyboardKey.Left) && (direction == up || direction == down) ) direction = left;
+  if (IsKeyDown(KeyboardKey.Up) && (direction == right || direction == left) ) direction = up;
+  if (IsKeyDown(KeyboardKey.Down) && (direction == right || direction == left) ) direction = down;
 
+  // -- Update Model --
+  if (frame % 5 == 0)
+  {
+    snake.RemoveAt(snake.Count - 1);
+    var head = snake.First();
+    snake.Insert(0, head + direction);
+  }
+
+  // -- Draw Frame --
   BeginDrawing();
-  Draw();
+  ClearBackground(Color.White);
+  var i = 0;
+  foreach (var snakebit in snake)
+  {
+    var color = Color.Black;
+    DrawRectangleV(snakebit, new Vector2(30, 30), color);
+    i++;
+  }
+  DrawFPS(0, 0);
+  DrawText($"{direction}", 0, 20, 20, Color.DarkGreen);
+  DrawText($"{snake.FirstOrDefault()}", 0, 40, 20, Color.Red);
   EndDrawing();
+  frame++;
 }
 
 CloseWindow();
-
-void Draw()
-{
-  ClearBackground(Color.RayWhite);
-  DrawRectangleV(square.position, square.size, square.color);
-  DrawFPS(screenWidth - MeasureText("XX FPS", 20), 0);
-  DrawText($"{square.position}", screenWidth / 2, screenHeight / 2, 20, Color.Blue);
-  if (!square.alive) DrawText("Dead", 0, 0, 40, Color.Red);
-}
-
-void Update()
-{
-  if (square.alive)
-  {
-
-    if (IsKeyDown(KeyboardKey.Right)) square.speed = new Vector2(speed, 0);
-    if (IsKeyDown(KeyboardKey.Left)) square.speed = new Vector2(-speed, 0);
-    if (IsKeyDown(KeyboardKey.Up)) square.speed = new Vector2(0, -speed);
-    if (IsKeyDown(KeyboardKey.Down)) square.speed = new Vector2(0, speed);
-
-    square.position += square.speed;
-
-    if (square.position.X < 0 || square.position.Y < 0 || square.position.X > screenWidth || square.position.Y > screenHeight) square.alive = false;
-  }
-  else
-  {
-    square.speed = new Vector2(0);
-    if (IsKeyPressed(KeyboardKey.Enter))
-    {
-      square.position = new Vector2(0);
-      square.speed = new Vector2(speed, 0);
-      square.alive = true;
-    }
-  }
-}
